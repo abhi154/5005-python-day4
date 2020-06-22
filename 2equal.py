ab3a5934c5957fd9d6139136586749bdd0c56dab
abhi154 <61627173+abhi154@users.noreply.github.com>
2020-06-22 10:45:30 +0530

    Add files via upload

---
+++ 2equal.py
@@ -0,0 +1,8 @@
+a=int(input("enter a value "))
+b=int(input("enter b value "))
+if a+b==5:
+	print("equals 5")
+elif a+b>5:
+	print("greater than 5")
+else :
+	print("less than 5")
---
+++ pass.py
@@ -0,0 +1,5 @@
+a=int(input("enter student marks"))
+if a>=35:
+	print("pass")
+else:
+	print("fail")
